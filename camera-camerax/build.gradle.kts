c7a214f3e9ff291cad9210055a369c8d57908679
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-19 13:01:57 +0530

    Add CameraX preview controller dependencies

--- camera-camerax/build.gradle.kts
+++ camera-camerax/build.gradle.kts
@@ -23,4 +23,5 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
+    implementation(libs.kotlinx.coroutines.android)
 }
