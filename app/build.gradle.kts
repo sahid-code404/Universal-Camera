eee92837026ebac6ef4bdd2c7c8ac9dc7e156979
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-19 11:43:38 +0530

    Use built-in Kotlin in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
@@ -31,10 +30,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 dependencies {
