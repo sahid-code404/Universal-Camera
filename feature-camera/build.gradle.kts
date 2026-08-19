2eae8c5cba81010636270565eb87acb826e1298a
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-19 13:02:51 +0530

    Connect feature camera UI to CameraX preview module

--- feature-camera/build.gradle.kts
+++ feature-camera/build.gradle.kts
@@ -24,6 +24,7 @@ android {
 dependencies {
     implementation(project(":core-model"))
     implementation(project(":camera-capability"))
+    implementation(project(":camera-camerax"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
@@ -33,5 +34,6 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.camera.view)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
