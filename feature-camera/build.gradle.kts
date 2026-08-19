f2026a62c1801c4a43f5e5e4fb787cc4569caf66
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-19 16:34:06 +0530

    Use Compose Camera Viewfinder

--- feature-camera/build.gradle.kts
+++ feature-camera/build.gradle.kts
@@ -36,5 +36,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.viewfinder.compose)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
