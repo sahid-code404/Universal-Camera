a3af5cabd0267637254a9d975cb6c4cfe4d473de
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-19 13:38:42 +0530

    chore: inject direct Camera2 aux controller

--- app/src/main/kotlin/com/omnicam/app/OmniCamApplication.kt
+++ app/src/main/kotlin/com/omnicam/app/OmniCamApplication.kt
@@ -1,7 +1,7 @@
 package com.omnicam.app
 
 import android.app.Application
-import com.omnicam.camera.camerax.CameraXPreviewController
+import com.omnicam.camera.camerax.Camera2AuxPreviewController
 import com.omnicam.camera.capability.AndroidCameraCapabilityScanner
 import com.omnicam.camera.capability.CameraCapabilityScanner
 
@@ -11,5 +11,5 @@ class OmniCamApplication : Application() {
 
 class AppContainer(application: Application) {
     val cameraCapabilityScanner: CameraCapabilityScanner = AndroidCameraCapabilityScanner(application)
-    val cameraXPreviewController: CameraXPreviewController = CameraXPreviewController(application)
+    val camera2AuxPreviewController: Camera2AuxPreviewController = Camera2AuxPreviewController(application)
 }
