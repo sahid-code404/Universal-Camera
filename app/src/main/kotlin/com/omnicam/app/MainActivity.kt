1d6d6ac27a1630306ca9de7eba54633ae901a663
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-19 22:27:46 +0530

    Launch the DNG-only camera

--- app/src/main/kotlin/com/omnicam/app/MainActivity.kt
+++ app/src/main/kotlin/com/omnicam/app/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import com.omnicam.feature.camera.LightningCameraRoute
+import com.omnicam.feature.camera.DngCameraRoute
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +16,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             OmniCamTheme {
                 val container = (application as OmniCamApplication).appContainer
-                LightningCameraRoute(
+                DngCameraRoute(
                     scanner = container.cameraCapabilityScanner,
-                    controller = container.lightningRawController,
+                    controller = container.dngCameraController,
                 )
             }
         }
